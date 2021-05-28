069a14682eb81ff775552b130ce550c5c6c93594
Kalle-Elmdahl <75391187+Kalle-Elmdahl@users.noreply.github.com>
2021-05-28 13:08:11 +0200

    Comments in util package

--- src/main/java/se/kth/iv1350/hgt/util/PrintWriterComposition.java
+++ src/main/java/se/kth/iv1350/hgt/util/PrintWriterComposition.java
@@ -10,11 +10,20 @@ import java.time.LocalTime;
  */
 public class PrintWriterComposition {
     private PrintWriter writer;
-
+    
+    /**
+     * Creates a new instance of the PrintWriterInhertance class
+     * @throws IOException if the named file exists but is a directory rather than a regular file, 
+     * does not exist but cannot be created, or cannot be opened for any other reason
+     */
     public PrintWriterComposition() throws IOException {
         writer = new PrintWriter(new FileWriter("total-revenue-compotion.txt"), true);
     }
 
+    /**
+     * Prints a message to a file and current date and time the function also adds a new line at the end
+     * @param message the message to be printed in the file
+     */
     public void println(String message) {
         LocalTime timeOfWrite = LocalTime.now();
         writer.println("Print from " + timeOfWrite.getHour() + ":" + timeOfWrite.getMinute() + " \n" + message);
--- src/main/java/se/kth/iv1350/hgt/util/PrintWriterInhertance.java
+++ src/main/java/se/kth/iv1350/hgt/util/PrintWriterInhertance.java
@@ -12,12 +12,17 @@ import java.time.LocalTime;
 public class PrintWriterInhertance extends PrintWriter {
     /**
      * Creates a new instance of the PrintWriterInhertance class
-     * @throws IOException if the named file exists but is a directory rather than a regular file, does not exist but cannot be created, or cannot be opened for any other reason
+     * @throws IOException if the named file exists but is a directory rather than a regular file, 
+     * does not exist but cannot be created, or cannot be opened for any other reason
      */
     public PrintWriterInhertance() throws IOException {
         super(new FileWriter("total-revenue-inheritance.txt"), true);
     }
 
+    /**
+     * Prints a message to a file and current date and time the function also adds a new line at the end
+     * @param message the message to be printed in the file
+     */
     @Override
     public void println(String message) {
         LocalTime timeOfWrite = LocalTime.now();
